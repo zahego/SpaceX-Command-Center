9d402aae9ee0824f1b5fa7f9c56cde48e45721fb
zahego <tug70814@temple.edu>
2020-08-23 00:45:21 -0400

    remove build from gitignore

---
+++ build/precache-manifest.c4b7b78f8f96807820ec6096f5ece10e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e384f0a07209b468c2c4348cdf468752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e39ee7e8053980f8f546",
+    "url": "/static/css/main.a512d6e9.chunk.css"
+  },
+  {
+    "revision": "d36d81bc7a3109b0dda1",
+    "url": "/static/js/2.b0310434.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0310434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39ee7e8053980f8f546",
+    "url": "/static/js/main.b76b518e.chunk.js"
+  },
+  {
+    "revision": "a4a1605d6aaa1151d46b",
+    "url": "/static/js/runtime-main.9a5898d1.js"
+  }
+]);
